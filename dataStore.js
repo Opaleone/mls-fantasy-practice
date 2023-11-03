f02f91d2c7bcb0f32cc6af68e1ab9c25af3286d1
Leone Varando <lvarando@gmail.com>
2023-11-03 18:22:56 -0500

    Added new version of player retrieval algo; Old algo in dataStore file

---
+++ dataStore.js
@@ -0,0 +1,83 @@
+/*
+  {
+    "apiId": 1099,
+    "firstName": "Alex",
+    "lastName": "Ring",
+    "displayName": "A. Ring",
+    "number": "9",
+    "age": "32",
+    "img": "https://b.fssta.com/uploads/application/soccer/headshots/23292.png",
+    "nationality": "Finland",
+    "team": "Austin FC",
+    "height": "1.78m",
+    "weight": "98kg",
+    "injured": "false",
+    "position": "Midfielder"
+  }
+*/
+
+
+const teamsUrl = `https://api-football-v1.p.rapidapi.com/v3/teams?league=${mlsId}&season=${seasonId}`;
+const teams = await fetch(teamsUrl, options);
+const teamResult = await teams.json();
+const teamInfo = await teamResult.response;
+const teamIds = [];
+
+for (let i = 0; i < teamInfo.length; i++) {
+  teamIds.push(teamInfo[i].team.id);
+}
+
+const playersStats = [];
+let page = 1;
+
+for (let i = 0; i < teamIds.length; i++) {
+  // Trying to grab player positions and numbers
+  const squadUrl = `https://api-football-v1.p.rapidapi.com/v3/players/squads?team=${teamIds[i]}`;
+  const squad = await fetch(squadUrl, options);
+  const squadResult = await squad.json();
+  const squadRes = squadResult.response[0];
+
+  for (let j = 0; j < squadRes.players.length; j++) {
+    playersStats.push(squadRes.players[j]);
+  }
+}
+
+let totalPlayerInfo = [];
+
+for (let i = 0; i < teamIds.length; i++) {
+  const playersUrl = `https://api-football-v1.p.rapidapi.com/v3/players?team=${teamIds[i]}&season=${seasonId}&page=${page}`;
+  const players = await fetch(playersUrl, options);
+  const playersResult = await players.json();
+  const playerInfo = await playersResult.response;
+
+  if (page < playersResult.paging.total) {
+    page++;
+    i--;
+  } else if (playersResult.paging.current === playersResult.paging.total) page = 1;
+
+  for (let j = 0; j < playerInfo.length; j++) {
+    totalPlayerInfo.push(playerInfo[j]);
+  }
+}
+
+playersStats.sort((a,b) => a.id - b.id);
+totalPlayerInfo.sort((a,b) => a.player.id - b.player.id);
+
+for (let i = 0, j = 0; i < playersStats.length; i++, j++) {
+  const objectInsertion = playersStats[i]
+  const curPlayerInfo = totalPlayerInfo[j].player;
+  const curPlayerStats = totalPlayerInfo[j].statistics[0];
+  if (objectInsertion.id === curPlayerInfo.id) {
+    objectInsertion.firstName = curPlayerInfo.firstname;
+    objectInsertion.lastName = curPlayerInfo.lastname;
+    objectInsertion.nationality = curPlayerInfo.nationality;
+    objectInsertion.team = curPlayerStats.team.name;
+    objectInsertion.height = curPlayerInfo.height;
+    objectInsertion.weight = curPlayerInfo.weight;
+    objectInsertion.injured = curPlayerInfo.injured;
+  } else {
+    i--;
+  }
+}
+
+console.log(playersStats);
